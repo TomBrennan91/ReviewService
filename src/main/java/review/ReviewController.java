98ef7dfc299dfd8687c815ab3781551b827b3a2a
TomBrennan91 <tombrennan@synapseinformation.com>
2019-01-21 15:54:47 +0000

    accept sorting param

--- src/main/java/review/ReviewController.java
+++ src/main/java/review/ReviewController.java
@@ -13,48 +13,12 @@ public class ReviewController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-//    @CrossOrigin
-//    @RequestMapping("/review")
-//    public ArrayList<Review> review(@RequestParam(value="titles", defaultValue="Empty Title") String input){
-//        //return new Review(counter.incrementAndGet(),
-//        //        String.format(template, name));
-//        System.out.println(input);
-//
-//
-//        String titles[] = input.split("~");
-//
-//        System.out.println(titles[titles.length -1]);
-//
-//        ArrayList<Review> reviews = new ArrayList<>();
-//
-//
-//        for (String title : titles){
-//            try {
-//                Review review = Review.getReviewFromTitle(title);
-//                //System.out.println(review.toString());
-//                if (review.getImdbRating() != null) {
-//                    reviews.add(review);
-//                } else {
-//                    System.err.println(title);
-//                }
-//            } catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        System.out.println(titles.length + " -> " + reviews.size());
-//        Collections.sort(reviews , (a,b) -> b.getImdbRating().compareToIgnoreCase(a.getImdbRating()));
-//        reviews.forEach(System.out::println);
-//        return reviews;
-//    }
-
-
     @CrossOrigin
     @PostMapping("/review")
-    public ArrayList<Review> reviews(@RequestBody String input){
+    public ArrayList<Review> reviews(@RequestBody String input, @RequestParam(value = "sort", defaultValue = "")String sorting){
 
+        System.out.println("sorting = " + sorting);
         System.out.println(input);
-
         String titles[] = input.split("~");
 
         System.out.println(titles[titles.length -1]);
