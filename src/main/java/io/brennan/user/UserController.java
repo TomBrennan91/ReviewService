1b4835e91faf5f659440b899b9d3b7370181c94b
Tom Brennan <tom-brennan@hotmail.co.uk>
2019-10-11 14:57:12 +0100

    share list with friends

--- src/main/java/io/brennan/user/UserController.java
+++ src/main/java/io/brennan/user/UserController.java
@@ -1,6 +1,5 @@
 package io.brennan.user;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.brennan.review.Review;
